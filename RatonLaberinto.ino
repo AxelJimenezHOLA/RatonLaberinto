d3f213aec23f995e1a58f78a2310ec13208b7ce4
Axel <axel.jimenez85@uabc.edu.mx>
2026-01-15 12:11:01 -0800

    este es el chilo

--- RatonLaberinto.ino
+++ RatonLaberinto.ino
@@ -28,59 +28,23 @@
 /* CONFIGURACIONES */
 #define FREQUENCY_PWM 4000
 #define RESOLUTION_PWM 8
-int speed = 0;
+#define BASE_SPEED 220
 
 /* PID */
-int YST = 2000;
-double KP = 0.095;
-double KI = 0;
-double KD = 0.0055;
+#define KP 0.6
+#define KI 0
+#define KD 2.2
+#define YST 1200
 
 int y = 0;
-double u = 0;
-double errorP = 0;
-double errorI = 0;
-double errorD = 0;
-double previousError = 0;
+float u = 0;
 
-/* CONTROL REMOTO */
-#define REMOTEXY__DEBUGLOG
-#define REMOTEXY_MODE__ESP32CORE_BLE
-
-#include <BLEDevice.h>
-
-#define REMOTEXY_BLUETOOTH_NAME "RatonLaberinto"
-
-#include <RemoteXY.h>
-
-#pragma pack(push, 1)  
-uint8_t const PROGMEM RemoteXY_CONF_PROGMEM[] =   // 114 bytes V19 
-  { 255,17,0,0,0,107,0,19,0,0,0,76,97,98,101,114,105,110,116,111,
-  65,76,0,69,2,106,200,200,84,1,1,5,0,7,8,78,24,24,44,10,
-  63,10,110,64,2,26,2,7,7,5,86,24,24,109,22,40,10,110,64,2,
-  26,2,1,7,5,138,24,24,44,22,63,10,110,64,2,26,2,7,7,68,
-  33,24,24,109,10,40,10,110,64,2,26,2,1,10,26,110,57,57,44,42,
-  105,31,49,4,1,31,79,78,0,31,79,70,70,0 };
-  
-// this structure defines all the variables and events of your control interface 
-struct {
-
-    // input variables
-  float edit_kp;
-  float edit_yst;
-  float edit_kd;
-  float edit_speed;
-  uint8_t pushSwitch_01; // =1 if state is ON, else =0, from 0 to 1
-
-    // other variable
-  uint8_t connect_flag;  // =1 if wire connected, else =0
-
-} RemoteXY;   
-
-#pragma pack(pop)
+int errorP = 0;
+int errorI = 0;
+int errorD = 0;
+int previousError = 0;
 
 void setup() {
-  RemoteXY_Init();
   Serial.begin(115200);
   Serial.println("Iniciando");
 
@@ -99,19 +63,54 @@ void setup() {
   // Inicializar pines digitales
   pinMode(PUSH, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
-  pinMode(LED_ON, OUTPUT);
 
-  digitalWrite(LED,1);
-  //secuenciaInicio();
+  // Secuencia de inicio
+  digitalWrite(LED, HIGH);
+  while (digitalRead(PUSH)) {
+    irTest();
+    updatePID();
+    setMotors(u, -u);
+  }
+
+  for (int k = 0; k < 3; k++) {
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
 }
 
 void loop() {
-  RemoteXYEngine.handler(); 
-  updateRemote();
   updatePID();
-  changeSpeedWithPWM();
+  setMotors(BASE_SPEED + u, BASE_SPEED - u);
 }
 
+/* FUNCIONES PID */
+void updatePID() {
+  int ir[] = {analogRead(IR1), analogRead(IR2)};
+  int sum = 0;
+  int n = sizeof(ir)/sizeof(ir[0]);
+  for (int i = 0; i < n; i++) sum += ir[i];
+
+  y = sum / n;
+
+  // Errores calculados
+  errorP = YST - y;
+  errorI += errorP;
+  errorD = errorP - previousError;
+
+  u = KP * errorP + KI * errorI + KD * errorD;
+  previousError = errorP;
+}
+
+void changeSpeedWithPID() {
+  if (u > 0)
+    setMotors(BASE_SPEED, BASE_SPEED - u);
+  else
+    setMotors(BASE_SPEED + u, BASE_SPEED);
+}
+
+/* FUNCIONES DE MOTOR */
 void setMotors(int leftMotorSpeed, int rightMotorSpeed) {
   setMotor(MOTOR_LEFT_POSITIVE, MOTOR_LEFT_NEGATIVE, leftMotorSpeed);
   setMotor(MOTOR_RIGHT_POSITIVE, MOTOR_RIGHT_NEGATIVE, rightMotorSpeed);
@@ -132,52 +131,8 @@ void setMotor(int motorPin1, int motorPin2, int speed) {
   }
 }
 
-void updateRemote() {
-  KP = RemoteXY.edit_kp * RemoteXY.pushSwitch_01;
-  KD = RemoteXY.edit_kd * RemoteXY.pushSwitch_01;
-  speed = RemoteXY.edit_speed * RemoteXY.pushSwitch_01;
-  YST = RemoteXY.edit_yst * RemoteXY.pushSwitch_01;
-}
-
-void updatePID() {
-  y = analogRead(IR2);
-
-  // Errores calculados
-  errorP = YST - y;
-  errorI += errorP;
-  errorD = errorP - previousError;
-
-  u = KP * errorP + KI * errorI + KD * errorD;
-  previousError = errorP;
-}
-
-void changeSpeedWithPWM() {
-  if (u > 0)
-    setMotors(speed, speed - u);
-  else
-    setMotors(speed + u, speed);
-}
-
-void secuenciaInicio() {
-  digitalWrite(LED, HIGH);
-  while (digitalRead(PUSH)) {
-    //irDebugPrint();
-    updatePID();
-    setMotors(u, -u);
-  }
-
-  for (int k = 0; k < 3; k++) {
-    digitalWrite(LED, HIGH);
-    delay(500);
-    digitalWrite(LED, LOW);
-    delay(500);
-  }
-}
-/*
-void irDebugPrint() {
-  digitalWrite(LED_ON, 1);
-  Serial.print(analogRead(BATTERY) * 0.000805664062); // El valor equivale a 3.3 / 4096
-  Serial.print("\t");
+/* FUNCIONES DE PRUEBA */
+void irTest() {
   Serial.print(analogRead(IR0));
   Serial.print("\t");
   Serial.print(analogRead(IR1));
@@ -190,7 +145,7 @@ void irDebugPrint() {
   Serial.print("\t");
   Serial.print(analogRead(IR5));
   Serial.print("\t");
-  Serial.println(analogRead(IR6));
-  digitalWrite(LED_ON, 0);
+  Serial.print(analogRead(IR6));
+  Serial.print("\t");
+  Serial.println(y);
 }
-*/
